8bc9257350943f130be8b3fa3adca08bc3e760ae
rabTAI <rabinofficial@hotmail.com>
2023-03-03 23:41:17 -0700

    headless server

--- functions/getSignedMessage.js
+++ functions/getSignedMessage.js
@@ -1,21 +1,21 @@
-const fetch = require("node-fetch");
+const { ethers } = require("ethers");
 
-exports.handler = async function (event, context) {
-    const response = await fetch(`${process.env.SERVER_ADDRESS}/getSignedMessage`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: event.body
-    })
-    // above this is the front end requesting the express server to check stuff or whatever
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
-    // below this is the response from the express server
-
-    const json = await response.json()
-
-    return {
-        body: JSON.stringify(json),
-        statusCode: 200,
-    };
-}
+//When FrontEnd requests a signed message
+export const func = async (req, res) => {
+    const {
+        code: message // The secret code to sign
+    } = (req.body)
+    try {
+        // Create a wallet to sign the hash with
+        let wallet = new ethers.Wallet(DEPLOYER_PRIVATE_KEY);
+        console.log(wallet.address);
+        // Sign the binary data
+        let sig = await wallet.signMessage(message);
+        return res.json({ signature: sig });
+    } catch (err) {
+        console.log("ERR ", err)
+        return res.json({ signature: err });
+    }
+};
