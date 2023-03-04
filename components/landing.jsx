ee5ee413f1a27403df64c345a4b73ba12fe785c6
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2023-03-03 22:25:20 -0700

    misc changes, deploy for mintnft error

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,15 +1,22 @@
-
+import Image
+    from "next/image";
 export default function Landing({ setScreen }) {
     return (
         <>
-            <h1 className="text-6xl mx-6">Something about murals</h1>
+            <h1 className="text-6xl mx-6">Discover &</h1>
             <p className="text-2xl mx-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
+            <Image
+                src={"/explore.png"}
+                alt="Explore"
+                width={100}
+                height={100}
+                className="inline ml-2"
+            />
             <button
                 className="fixed inline-block text-lg group bottom-8"
                 onClick={() => setScreen('discover')}
             >
-                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                <span className="relative bg-primary z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                     <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
                     <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
                     <span className="relative">Discover Denver</span>
