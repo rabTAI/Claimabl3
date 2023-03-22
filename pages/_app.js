ede3d16443621272fad19a7ed2579367cfca71de
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2023-03-22 16:49:53 -0400

    webmodal package naming changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,54 @@
-
-import "../styles/globals.css";
-import 'leaflet/dist/leaflet.css'
-import Head from "next/head";
-import MainLayout from "../layout/mainLayout";
-import { ethers } from "ethers";
+import '../styles/globals.css';
+import 'leaflet/dist/leaflet.css';
+import Head from 'next/head';
+import MainLayout from '../layout/mainLayout';
+import { ethers } from 'ethers';
 
 import {
-	EthereumClient,
-	modalConnectors,
-	walletConnectProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { baseGoerli } from "wagmi/chains";
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/react';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { baseGoerli } from 'wagmi/chains';
 
 const chains = [baseGoerli];
 
 // Wagmi client
 const { provider } = configureChains(chains, [
-	walletConnectProvider({ projectId: "bc48326f8b7d0f6c85abe369f8016774" }),
+  w3mProvider({ projectId: 'bc48326f8b7d0f6c85abe369f8016774' }),
 ]);
 
 const wagmiClient = createClient({
-	autoConnect: true,
-	connectors: modalConnectors({
-		projectId: "bc48326f8b7d0f6c85abe369f8016774",
-		version: "1", // or "2"
-		appName: "web3Modal",
-		chains,
-	}),
-	provider,
+  autoConnect: true,
+  connectors: w3mConnectors({
+    projectId: 'bc48326f8b7d0f6c85abe369f8016774',
+    version: '1', // or "2"
+    appName: 'web3Modal',
+    chains,
+  }),
+  provider,
 });
 
 // Web3Modal Ethereum Client
 const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<WagmiConfig client={wagmiClient}>
-				<MainLayout>
-					<Component {...pageProps} />
-				</MainLayout>
-			</WagmiConfig>
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </WagmiConfig>
 
-			<Web3Modal
-				projectId="bc48326f8b7d0f6c85abe369f8016774"
-				ethereumClient={ethereumClient}
-			/>
-		</>
-
-	);
+      <Web3Modal
+        projectId='bc48326f8b7d0f6c85abe369f8016774'
+        ethereumClient={ethereumClient}
+      />
+    </>
+  );
 }
 
 export default MyApp;
